b1a71c805096d6fb6df0cce16461403ac0894598
SanFabian <sanfabiian@gmail.com>
2021-05-31 14:25:51 -0500

    Ajustes en vista tablet

--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -1,31 +1,43 @@
 <script>
-    import ortodoncia from '@static/icon-ortodoncia.svg'
-    import endodoncia from '@static/icon-endodoncia.svg'
-    import periodoncia from '@static/icon-periodoncia.svg'
-    import diseno from '@static/icon-diseno.svg'
-    import Service from '@components/Service.svelte'
+  import ortodoncia from "@static/icon-ortodoncia.svg";
+  import endodoncia from "@static/icon-endodoncia.svg";
+  import periodoncia from "@static/icon-periodoncia.svg";
+  import diseno from "@static/icon-diseno.svg";
+  import Service from "@components/Service.svelte";
 </script>
+
 <section class="content">
-    <h1 class="title">Especialidades</h1>
-    <div class="wrap">
-        <Service service="Ortodoncia" slug="ortodoncia" img={ortodoncia} />
-        <Service service="endodoncia" slug="endodoncia" img={endodoncia} />
-        <Service service="periodoncia" slug="periodoncia" img={periodoncia} />
-        <Service service="Diseño de sonrisa" slug="diseno-de-sonrisa" img={diseno} />
-    </div>
+  <h1 class="title">Especialidades</h1>
+  <div class="wrap">
+    <Service service="Ortodoncia" slug="ortodoncia" img={ortodoncia} />
+    <Service service="endodoncia" slug="endodoncia" img={endodoncia} />
+    <Service service="periodoncia" slug="periodoncia" img={periodoncia} />
+    <Service
+      service="Diseño de sonrisa"
+      slug="diseno-de-sonrisa"
+      img={diseno}
+    />
+  </div>
 </section>
 
 <style>
-    .wrap {
-        text-align: center;
-        font-size: var(--size-mintext);
-    }
+  .wrap {
+    text-align: center;
+    font-size: var(--size-mintext);
+  }
 
-    @media (min-width: 768px) {
-        .wrap {
-            display: grid;
-            grid-template-columns: repeat(4, 1fr);
-            gap: 20px;
-        }
+  @media (min-width: 768px) {
+    .wrap {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 20px;
     }
+  }
+  @media (min-width: 1024px) {
+    .wrap {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: 20px;
+    }
+  }
 </style>
--- src/components/Sitebar.svelte
+++ src/components/Sitebar.svelte
@@ -1,72 +1,76 @@
 <script>
-	import linksNav from '@utils/linksNav.js'
-    export let segment;
-	export let open = false
-	function btnOpen() {
-		open = !open
-	}
-
+  import linksNav from "@utils/linksNav.js";
+  export let segment;
+  export let open = false;
+  function btnOpen() {
+    open = !open;
+  }
 </script>
 
 <nav class:open>
-	<ul>
-		{#each linksNav as route}
-			<li>
-				<a
-					aria-current="{segment === route.name ? 'page' : undefined}" href={route.route}
-					on:click={btnOpen}
-				>
-					{route.name}
-				</a>
-			</li>
-		{/each}
-	</ul>
+  <ul>
+    {#each linksNav as route}
+      <li>
+        <a
+          aria-current={segment === route.name ? "page" : undefined}
+          href={route.route}
+          on:click={btnOpen}
+        >
+          {route.name}
+        </a>
+      </li>
+    {/each}
+  </ul>
 </nav>
+
 <style>
-	ul {
-		text-align: right;
-	}
-	li {
-		list-style: none;
-	}
-	a {
-		display: block;
-		width: 100%;
-		padding: 10px 20px;
-		color: var(--white);
-		text-decoration: none;
-		font-family: var(--font-maven);
-		font-size: var(--size-text);
-		text-transform: capitalize;
-	}
-	nav {
-		position: absolute;
-		top: 65px;
-		width: 100%;
-		height: 100vh;
-		background: linear-gradient(var(--green), transparent), linear-gradient(90deg, #6197be, transparent), linear-gradient(-90deg, #61b0be, transparent );
-		background-color: white;
-		right: -100%;
-		transition: right 0.3s ease-in-out;
-		z-index: 1;
-	}
-	.open {
-		right: 0;
-	}
-	@media (min-width: 768px) {
-		nav {
-			position: initial;
-			height: 65px;
-			background: var(--green);
-		}
-		nav ul {
-			display: flex;
-			justify-content: flex-end;
-			align-items: center;
-			height: 100%;
-		}
-		ul a {
-			padding-right: 0;
-		}
-	}
+  ul {
+    text-align: right;
+  }
+  li {
+    list-style: none;
+  }
+  a {
+    display: block;
+    width: 100%;
+    padding: 10px 20px;
+    color: var(--white);
+    text-decoration: none;
+    font-family: var(--font-maven);
+    font-size: var(--size-text);
+    text-transform: capitalize;
+  }
+  nav {
+    position: absolute;
+    top: 65px;
+    width: 100%;
+    height: 100vh;
+    background: linear-gradient(var(--green), transparent),
+      linear-gradient(90deg, #6197be, transparent),
+      linear-gradient(-90deg, #61b0be, transparent);
+    background-color: white;
+    right: -100%;
+    transition: right 0.3s ease-in-out;
+    z-index: 1;
+  }
+  .open {
+    right: 0;
+  }
+  @media (min-width: 768px) {
+    nav {
+      position: initial;
+      height: 65px;
+      background: var(--green);
+      padding: 0 30px;
+    }
+    nav ul {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      height: 100%;
+    }
+    ul a {
+      padding-right: 0;
+    }
+  }
 </style>
