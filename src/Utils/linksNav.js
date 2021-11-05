fd115e884187173ef71d3423fdf820ae0cfe670c
SanFabian <sanfabiian@gmail.com>
2021-11-05 08:49:20 -0500

    habilitada la galeria en beta

--- src/Utils/linksNav.js
+++ src/Utils/linksNav.js
@@ -3,7 +3,7 @@ const routesNavigation = [
   { name: "Nosotros", route: "nosotros" },
   { name: "Testimonios", route: "testimonios" },
   { name: "Financiación", route: "financiacion" },
-  // { name: "Galeria", route: "galeria" },
+  { name: "Galeria", route: "galeria" },
 ];
 
 export default routesNavigation;
